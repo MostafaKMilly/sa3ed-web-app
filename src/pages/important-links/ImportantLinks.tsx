6c8e331d78694bc520786b6d285c616dc1c1a7a8
Mostafa Milly <m.milly@work-with-nerds.ca>
2023-02-11 15:07:25 +0200

    Add importants link

--- src/pages/important-links/ImportantLinks.tsx
+++ src/pages/important-links/ImportantLinks.tsx
@@ -1,3 +1,112 @@
+import API from "@/api/httpClient";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { useQuery } from "@tanstack/react-query";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import { locationQuery } from "../donation/Donation";
+import LocationCityIcon from "@mui/icons-material/LocationCity";
+import DescriptionIcon from "@mui/icons-material/Description";
+import Divider from "@mui/material/Divider";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const importantLinkQuery = {
+  queryKey: ["Importants_Links"],
+  queryFn: () =>
+    API.get<Info[], { data: Info[] }>("importantinfo", (res) => res.data),
+};
+
 export const ImportantLinks = () => {
-  return <div></div>;
+  const { data, isLoading } = useQuery(importantLinkQuery);
+  const { data: location } = useQuery(locationQuery);
+
+  const getCity = (id: number) => {
+    return location?.find((city) => city.id === id)?.name;
+  };
+
+  if (isLoading) {
+    return (
+      <Box
+        width="100%"
+        display="flex"
+        flexDirection="column"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <CircularProgress />
+      </Box>
+    );
+  }
+
+  return (
+    <Box display="flex" flexDirection="column" rowGap={4}>
+      {data?.map((info) => (
+        <>
+          <Box display="flex" flexDirection="column" rowGap={3}>
+            <Typography variant="h3" fontWeight={700} gutterBottom>
+              {info.name}
+            </Typography>
+            <Box display="flex" columnGap={1} textAlign="center">
+              <AccessTimeIcon
+                sx={{ color: info.available ? "success.main" : "error.main" }}
+              />
+              <Typography
+                variant="h4"
+                sx={{ color: info.available ? "success.main" : "error.main" }}
+              >
+                {info.available ? "متاح" : "غير متاح"}
+              </Typography>
+            </Box>
+            <Box display="flex" columnGap={1} textAlign="center">
+              <Typography
+                sx={{
+                  color: "common.black",
+                  display: "flex",
+                  columnGap: 1,
+                  textAlign: "center",
+                }}
+                variant="h4"
+                fontWeight={700}
+              >
+                <LocationCityIcon fontSize="small" /> المحافظة :
+              </Typography>
+              <Typography variant="body1">
+                {getCity(info.id_city as number)}
+              </Typography>
+            </Box>
+            <Box display="flex" columnGap={1} textAlign="center">
+              <Typography
+                sx={{
+                  color: "common.black",
+                  display: "flex",
+                  columnGap: 1,
+                  textAlign: "center",
+                }}
+                variant="h4"
+                fontWeight={700}
+              >
+                <DescriptionIcon fontSize="small" /> مزيد من المعلومات :
+              </Typography>
+              <Typography variant="body1" textAlign="left">
+                {info.description}
+              </Typography>
+            </Box>
+          </Box>
+          <Divider />
+        </>
+      ))}
+    </Box>
+  );
+};
+
+type Info = {
+  id: number;
+  id_city: number;
+  id_area: null;
+  name: string;
+  available: boolean;
+  location_details: string;
+  info_type: number;
+  description: string;
+  created_at: string;
+  updated_at: string;
 };
