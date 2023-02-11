aeeb50e7b7d130de347e1f0a76af074438ddbd2f
Mostafa Milly <m.milly@work-with-nerds.ca>
2023-02-11 04:44:37 +0200

    Try to fix build issue

---
+++ src/theme/modules.d.ts
@@ -0,0 +1,25 @@
+declare module "@mui/material/Button/Button" {
+  interface ButtonPropsColorOverrides {
+    tertiary: true;
+  }
+  interface ButtonPropsVariantOverrides {
+    tertiary: true;
+  }
+}
+
+declare module "@mui/material/styles/createPalette" {
+  export interface PaletteOptions {
+    tertiary?: PaletteColorOptions;
+  }
+  export interface Palette {
+    tertiary: PaletteColor;
+  }
+}
+
+declare module "@mui/material/Textfield/Textfield" {
+  export interface TextFieldPropsColorOverrides {
+    tertiary?: true;
+  }
+}
+
+export {}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,32 +1,12 @@
-import { createTheme, PaletteColor, PaletteColorOptions, Theme } from "@mui/material";
+import {
+  createTheme,
+  PaletteColor,
+  PaletteColorOptions,
+  Theme,
+} from "@mui/material";
 import { componentOverrides } from "./overrides";
 import { palette } from "./palette";
 import typography from "./typography";
-
-declare module "@mui/material/Button" {
-  interface ButtonPropsColorOverrides {
-    tertiary: true;
-  }
-  interface ButtonPropsVariantOverrides {
-    tertiary: true;
-  }
-}
-
-declare module "@mui/material/styles/createPalette" {
-  export interface PaletteOptions {
-    tertiary?: PaletteColorOptions;
-  }
-  export interface Palette {
-    tertiary: PaletteColor;
-  }
-}
-
-declare module "@mui/material/Textfield" {
-  export interface TextFieldPropsColorOverrides {
-    tertiary?: true;
-  }
-}
-
 
 export const theme = createTheme({
   palette,
