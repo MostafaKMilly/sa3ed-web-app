c396a401a8a891a8ad105f06b9173a4249ceacac
Mostafa Milly <m.milly@work-with-nerds.ca>
2023-02-11 04:13:26 +0200

    Add modules agumentation

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -3,30 +3,6 @@ import { componentOverrides } from "./overrides";
 import { palette } from "./palette";
 import typography from "./typography";
 
-declare module "@mui/material/Button/Button" {
-  interface ButtonPropsColorOverrides {
-    tertiary: true;
-  }
-  interface ButtonPropsVariantOverrides {
-    tertiary: true;
-  }
-}
-
-declare module "@mui/material/styles/createPalette" {
-  export interface PaletteOptions {
-    tertiary?: PaletteColorOptions;
-  }
-  export interface Palette {
-    tertiary: PaletteColor;
-  }
-}
-
-declare module "@mui/material/Textfield/Textfield" {
-  export interface TextFieldPropsColorOverrides {
-    tertiary?: true;
-  }
-}
-
 export const theme = createTheme({
   palette,
   direction: "rtl",
