7331dd21d7e64da7c650980290f65a588146e4d7
Mostafa Milly <m.milly@work-with-nerds.ca>
2023-02-11 14:44:49 +0200

    Add overlay

--- src/pages/user-info/components/UserRequests.tsx
+++ src/pages/user-info/components/UserRequests.tsx
@@ -1,8 +1,9 @@
 import API from "@/api/httpClient";
 import { HelpData } from "@/pages/help/types";
 import { useQuery } from "@tanstack/react-query";
-import { Box, Paper, alpha } from "@mui/material";
+import { Box, Paper, alpha, Typography } from "@mui/material";
 import UserRequestItem from "./UserRequestItem";
+import HistoryIcon from "@mui/icons-material/History";
 
 const requestQuery = {
   queryKey: ["Requests"],
@@ -18,10 +19,31 @@ export const UserRequests = () => {
   const { data: requests } = useQuery(requestQuery);
 
   return (
-    <Box display="flex" flexDirection="column" rowGap={3} width="100%">
+    <Box display="flex" flexDirection="column" rowGap={3} width="100%" mb={9}>
       {requests?.map((request) => (
         <UserRequestItem item={request} key={request.id} />
       ))}
+      {!requests?.length && (
+        <Box
+          display="flex"
+          flexDirection="column"
+          rowGap={2}
+          width="100%"
+          height="100%"
+          alignItems="center"
+          justifyContent="center"
+          p={2}
+        >
+          <HistoryIcon fontSize="large" sx={{ color: "primary.dark" }} />
+          <Typography
+            variant="h3"
+            fontWeight={700}
+            sx={{ color: "primary.dark" }}
+          >
+            لا يوجد بيانات لعرضها
+          </Typography>
+        </Box>
+      )}
     </Box>
   );
 };
