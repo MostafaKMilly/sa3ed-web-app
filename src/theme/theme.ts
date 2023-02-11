f1438086694d5303a5c8186e2b56e8b561812c60
Mostafa Milly <m.milly@work-with-nerds.ca>
2023-02-11 04:36:14 +0200

    Test

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -21,7 +21,7 @@ declare module "@mui/material/styles/createPalette" {
   }
 }
 
-declare module "@mui/material/Textfield/Textfield" {
+declare module "@mui/material/Textfield" {
   export interface TextFieldPropsColorOverrides {
     tertiary?: true;
   }
